7d9b1afef70b119782522c347f0f172ea74b8c03
KingVikraman <rajavikraman2006@gmail.com>
2025-08-11 22:10:23 +0800

    Feat & update : Sorted the main, for norm. Changed the printing code line.
    	• I have splitted the main to 5 diffrent functions, and they all serve
    	  a much more clearer purpose, its more easier to see with the eye.
    	• I have also found a new way to replace the two printf code line
    	  to just using one, this way is more efficent.
    	• Lastly, i will be also removing the older main after this commit.

--- src/main.c
+++ src/main.c
@@ -90,36 +90,94 @@ int	main(int argc, char **argv)
 
 static	int	validate_args(int args)
 {
-
+	if (args != 2)
+	{
+		ft_printf("Error\nUsage: ./so_long maps/map.ber\n");
+		return (0);
+	}
+	return (1);
 }
 
 
 static	int	init_game_structs(t_game *game, char *map_path)
 {
-
+	ft_memset(game, 0, sizeof(t_game));
+	if (!parse_map(map_path, game))
+	{
+		ft_printf("Error\nFailed to parse map\n");
+		return (0);
+	}
+	if (!validated_map(game->map, game))
+	{
+		free_map(game->map);
+		return (0);
+	}
+	return (1);
 }
 
 
 static	int	memory_allocation(t_game *game)
 {
+	int i;
 
+	game->collected_flags = malloc(sizeof(int *) * game->height);
+	if (!game->collected_flags)
+	{
+		free_map(game->map);
+		ft_printf("Error\nMemory allocation failure\n");
+		return (0);
+	}
+	i = -1;
+	while (++i < game->height)
+	{
+		game->collected_flags[i] = malloc(sizeof(int) * game->width);
+		if (!game->collected_flags)
+		{
+			while(--i >= 0)
+				free(game->collected_flags[i]);
+			free(game->collected_flags);
+			ft_printf("Error\nMemory allocation failure\n");
+			return (0);
+		}
+	}
+	init_collected_flags(game);
+	return (1);
 }
+
 
 static	int	game_lauch(t_game *game)
 {
-
+	if (!init_game(game))
+	{
+		free_resources(game);
+		ft_printf("Error\nMLX failed\n");
+		return (0);
+	}
+	if (!load_images(game))
+	{
+		free_resources(game);
+		return (0);
+	}
+	render_map(game);
+	mlx_hook(game->win, 17, 0, close_window, game);
+	mlx_key_hook(game->win, handle_key, game);
+	mlx_loop_hook(game->win, idle_check_loop, game);
+	mlx_loop(game->mlx);
+	return (1);
 }
 
 int	main(int argc, char **argv)
 {
-	if ()
+	t_game game;
+
+	if (!validate_args(argc))
 		return (1);
-	if ()
+	if (!init_game_structs(&game, argv[1]))
 		return (1);
-	if ()
+	if (!allocate_flags(&game))
 		return (1);
 	ft_printf("Map parsed and validated successfully\n");
-	if ()
+	if (!start_game(&game))
 		return (1); //failure
 	return (0); // succsess
 }
