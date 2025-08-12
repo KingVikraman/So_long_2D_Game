98d60d31a3c24d1a395860fe47027d56324d921b
KingVikraman <rajavikraman2006@gmail.com>
2025-08-12 20:56:45 +0800

    Update & Feat : Edited the code stucture for free_recources.
     • I splitted the 83 line function to smaller parts where the logic are the
       same just that its done little by little and one main function called
       free recousces that calles all the helper functions.
    
     • Mostly used static for norm fixes so that i can avoid overcomplicating the
       header file.
    
     • I also manage to find a new way to substitute the long line of if statements,
       its by calling another function for the if condition. Learned something new there.

--- src/free_function.c
+++ src/free_function.c
@@ -17,49 +17,98 @@ void	free_map(char **map)
 }
 
 
-void	free_resources(t_game *game)
-{
-	int	i;
+// void	free_resources(t_game *game)
+// {
+// 	int	i;
 
-	if (game->img_idle)
-		mlx_destroy_image(game->mlx, game->img_idle);
-	if (game->img_wall)
-		mlx_destroy_image(game->mlx, game->img_wall);
-	if (game->img_floor)
-		mlx_destroy_image(game->mlx, game->img_floor);
-	if (game->img_chest_closed)
-		mlx_destroy_image(game->mlx, game->img_chest_closed);
-	if (game->img_chest_open)
-		mlx_destroy_image(game->mlx, game->img_chest_open);
-	if (game->img_exit_closed)
-		mlx_destroy_image(game->mlx, game->img_exit_closed);
-	if (game->img_exit_open) 
-		mlx_destroy_image(game->mlx, game->img_exit_open);
-	if (game->img_run_up_1)
-		mlx_destroy_image(game->mlx, game->img_run_up_1);
-	if (game->img_run_up_2)
-		mlx_destroy_image(game->mlx, game->img_run_up_2);
-	if (game->img_run_down_1)
-		mlx_destroy_image(game->mlx, game->img_run_down_1);
-	if (game->img_run_down_2)
-		mlx_destroy_image(game->mlx, game->img_run_down_2);
-	if (game->img_run_left_1)
-		mlx_destroy_image(game->mlx, game->img_run_left_1);
-	if (game->img_run_left_2)
-		mlx_destroy_image(game->mlx, game->img_run_left_2);
-	if (game->img_run_right_1)
-		mlx_destroy_image(game->mlx, game->img_run_right_1);
-	if (game->img_run_right_2)
-		mlx_destroy_image(game->mlx, game->img_run_right_2);
+// 	if (game->img_idle)
+// 		mlx_destroy_image(game->mlx, game->img_idle);
+// 	if (game->img_wall)
+// 		mlx_destroy_image(game->mlx, game->img_wall);
+// 	if (game->img_floor)
+// 		mlx_destroy_image(game->mlx, game->img_floor);
+// 	if (game->img_chest_closed)
+// 		mlx_destroy_image(game->mlx, game->img_chest_closed);
+// 	if (game->img_chest_open)
+// 		mlx_destroy_image(game->mlx, game->img_chest_open);
+// 	if (game->img_exit_closed)
+// 		mlx_destroy_image(game->mlx, game->img_exit_closed);
+// 	if (game->img_exit_open) 
+// 		mlx_destroy_image(game->mlx, game->img_exit_open);
+// 	if (game->img_run_up_1)
+// 		mlx_destroy_image(game->mlx, game->img_run_up_1);
+// 	if (game->img_run_up_2)
+// 		mlx_destroy_image(game->mlx, game->img_run_up_2);
+// 	if (game->img_run_down_1)
+// 		mlx_destroy_image(game->mlx, game->img_run_down_1);
+// 	if (game->img_run_down_2)
+// 		mlx_destroy_image(game->mlx, game->img_run_down_2);
+// 	if (game->img_run_left_1)
+// 		mlx_destroy_image(game->mlx, game->img_run_left_1);
+// 	if (game->img_run_left_2)
+// 		mlx_destroy_image(game->mlx, game->img_run_left_2);
+// 	if (game->img_run_right_1)
+// 		mlx_destroy_image(game->mlx, game->img_run_right_1);
+// 	if (game->img_run_right_2)
+// 		mlx_destroy_image(game->mlx, game->img_run_right_2);
 	
-	free_map(game->map);
-	if (game->collected_flags)
-	{
-		i = 0;
-		while (i < game->height)
-			free(game->collected_flags[i++]);
-		free(game->collected_flags);
-	}
+// 	free_map(game->map);
+// 	if (game->collected_flags)
+// 	{
+// 		i = 0;
+// 		while (i < game->height)
+// 			free(game->collected_flags[i++]);
+// 		free(game->collected_flags);
+// 	}
+// 	if (game->win)
+// 		mlx_destroy_window(game->mlx, game->win);
+// 	if (game->mlx)
+// 	{
+// 		mlx_destroy_display(game->mlx);
+// 		free(game->mlx);
+// 	}
+// }
+
+
+static	void clear_images(void *mlx, void *img)
+{
+	if (img)
+		mlx_destroy_image(mlx, img);
+}
+
+static	void destroy_images(t_game *game)
+{
+	clear_images(game->mlx, game->img_idle);
+	clear_images(game->mlx, game->img_wall);
+	clear_images(game->mlx, game->img_floor);
+	clear_images(game->mlx, game->img_chest_closed);
+	clear_images(game->mlx, game->img_chest_open);
+	clear_images(game->mlx, game->img_exit_closed);
+	clear_images(game->mlx, game->img_exit_open);
+	clear_images(game->mlx, game->img_run_up_1);
+	clear_images(game->mlx, game->img_run_up_2);
+	clear_images(game->mlx, game->img_run_down_1);
+	clear_images(game->mlx, game->img_run_down_2);
+	clear_images(game->mlx, game->img_run_left_1);
+	clear_images(game->mlx, game->img_run_left_2);
+	clear_images(game->mlx, game->img_run_right_1);
+	clear_images(game->mlx, game->img_run_right_2);
+}
+
+static	void free_collected_flags(t_game *game)
+{
+	int i;
+
+	if (!game->collected_flags)
+		return ;
+	i = 0;
+	while (i < game->height)
+		free(game->collected_flags[i++]);
+	free(game->collected_flags);
+}
+
+static	void destroy_window_and_mlx(t_game *game)
+{
 	if (game->win)
 		mlx_destroy_window(game->mlx, game->win);
 	if (game->mlx)
@@ -67,4 +116,12 @@ void	free_resources(t_game *game)
 		mlx_destroy_display(game->mlx);
 		free(game->mlx);
 	}
+}
+
+void	free_resources(t_game *game)
+{
+	destroy_images(game);
+	free_map(game->map);
+	free_collected_flags(game);
+	destroy_window_and_mlx(game);
 }
--- src/load_images.c
+++ src/load_images.c
@@ -1,46 +1,6 @@
 
 #include "../includes/so_long.h"
 
-// int	load_images(t_game *r)
-// {
-// 	int	w;
-// 	int	h;
-
-// 	r->img_wall = mlx_xpm_file_to_image(r->mlx, "assets/wall.xpm", &w, &h);
-// 	r->img_floor = mlx_xpm_file_to_image(r->mlx, "assets/floor.xpm", &w, &h);
-// 	r->img_chest_closed = mlx_xpm_file_to_image(r->mlx, "assets/Collectible/closed_chest.xpm", &w, &h);
-// 	r->img_chest_open = mlx_xpm_file_to_image(r->mlx, "assets/Collectible/open_chest.xpm", &w, &h);
-// 	r->img_exit_closed = mlx_xpm_file_to_image(r->mlx, "assets/exit_door_close.xpm", &w, &h);
-// 	r->img_exit_open = mlx_xpm_file_to_image(r->mlx, "assets/exit_door_open.xpm", &w, &h);
-// 	r->img_idle = mlx_xpm_file_to_image(r->mlx, "assets/idle_player.xpm", &w, &h);
-
-
-// 	//  Player animations
-
-// 	r->img_run_up_1      = mlx_xpm_file_to_image(r->mlx, "assets/Player/run_up_1.xpm", &w, &h);
-// 	r->img_run_up_2      = mlx_xpm_file_to_image(r->mlx, "assets/Player/run_up_2.xpm", &w, &h);
-// 	r->img_run_down_1    = mlx_xpm_file_to_image(r->mlx, "assets/Player/run_down_1.xpm", &w, &h);
-// 	r->img_run_down_2    = mlx_xpm_file_to_image(r->mlx, "assets/Player/run_down_2.xpm", &w, &h);
-// 	r->img_run_left_1    = mlx_xpm_file_to_image(r->mlx, "assets/Player/run_left_1.xpm", &w, &h);
-// 	r->img_run_left_2    = mlx_xpm_file_to_image(r->mlx, "assets/Player/run_left_2.xpm", &w, &h);
-// 	r->img_run_right_1   = mlx_xpm_file_to_image(r->mlx, "assets/Player/run_right_1.xpm", &w, &h);
-// 	r->img_run_right_2   = mlx_xpm_file_to_image(r->mlx, "assets/Player/run_right_2.xpm", &w, &h);
-
-// 	if (!r->img_wall || !r->img_floor || !r->img_chest_closed || !r->img_chest_open ||
-// 		!r->img_exit_closed || !r->img_exit_open ||
-// 		!r->img_run_up_1 || !r->img_run_up_2 ||
-// 		!r->img_run_down_1 || !r->img_run_down_2 ||
-// 		!r->img_run_left_1 || !r->img_run_left_2 ||
-// 		!r->img_run_right_1 || !r->img_run_right_2 || !r->img_idle)
-// 	{
-// 		ft_printf("Error\n");
-// 		ft_printf("Failed to load one or more sprites\n");
-// 		free_resources(r);
-// 		return (0);
-// 	}
-// 	return (1);
-// }
-
 
 static	void load_environment_images(t_game *game, int *w, int *h)
 {
@@ -60,22 +20,14 @@ static	void load_collectible_images(t_game *game, int *w, int *h)
 static void	load_player_images(t_game *game, int *w, int *h)
 {
 	game->img_idle = mlx_xpm_file_to_image(game->mlx, "assets/idle_player.xpm", w, h);
-	game->img_run_up_1 = mlx_xpm_file_to_image(game->mlx,
-			"assets/Player/run_up_1.xpm", w, h);
-	game->img_run_up_2 = mlx_xpm_file_to_image(game->mlx,
-			"assets/Player/run_up_2.xpm", w, h);
-	game->img_run_down_1 = mlx_xpm_file_to_image(game->mlx,
-			"assets/Player/run_down_1.xpm", w, h);
-	game->img_run_down_2 = mlx_xpm_file_to_image(game->mlx,
-			"assets/Player/run_down_2.xpm", w, h);
-	game->img_run_left_1 = mlx_xpm_file_to_image(game->mlx,
-			"assets/Player/run_left_1.xpm", w, h);
-	game->img_run_left_2 = mlx_xpm_file_to_image(game->mlx,
-			"assets/Player/run_left_2.xpm", w, h);
-	game->img_run_right_1 = mlx_xpm_file_to_image(game->mlx,
-			"assets/Player/run_right_1.xpm", w, h);
-	game->img_run_right_2 = mlx_xpm_file_to_image(game->mlx,
-			"assets/Player/run_right_2.xpm", w, h);
+	game->img_run_up_1 = mlx_xpm_file_to_image(game->mlx, "assets/Player/run_up_1.xpm", w, h);
+	game->img_run_up_2 = mlx_xpm_file_to_image(game->mlx, "assets/Player/run_up_2.xpm", w, h);
+	game->img_run_down_1 = mlx_xpm_file_to_image(game->mlx, "assets/Player/run_down_1.xpm", w, h);
+	game->img_run_down_2 = mlx_xpm_file_to_image(game->mlx, "assets/Player/run_down_2.xpm", w, h);
+	game->img_run_left_1 = mlx_xpm_file_to_image(game->mlx, "assets/Player/run_left_1.xpm", w, h);
+	game->img_run_left_2 = mlx_xpm_file_to_image(game->mlx, "assets/Player/run_left_2.xpm", w, h);
+	game->img_run_right_1 = mlx_xpm_file_to_image(game->mlx, "assets/Player/run_right_1.xpm", w, h);
+	game->img_run_right_2 = mlx_xpm_file_to_image(game->mlx, "assets/Player/run_right_2.xpm", w, h);
 }
 
 static int	validate_images(t_game *game)
